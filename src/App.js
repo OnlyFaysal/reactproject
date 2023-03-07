32fc3ecab9cdded08976e81f3330f5dc90fb906c
Faysal <Faisalalkubaisi1999@gmail.com>
2023-03-08 02:27:49 +0300

    forth push

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src="logo192.png"alt='aletr'></img>
         <a
           className="App-link"
           href="https://reactjs.org"
